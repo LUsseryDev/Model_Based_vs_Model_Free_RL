39b237e2b87e25a4ac4544cd2f35266f76c10aab
prometheus <102935414+PROMETHEUS7777@users.noreply.github.com>
2025-11-07 03:24:08 -0500

    impala doesn't crash

--- Algorithms/IMPALA.py
+++ Algorithms/IMPALA.py
@@ -7,6 +7,7 @@ import gymnasium as gym
 import EnvUtil
 from MiscUtil import *
 import torch.multiprocessing as mp
+from torch.nn import functional as F
 
 
 class NeuralNet(torch.nn.Module):
@@ -42,7 +43,11 @@ def actor(game:str, tqueue: mp.Queue, mqueue: mp.Queue, is_done:mp.Event(), is_p
 
     #main loop
     while True:
-        trajectory = []
+        obs_trajectory = []
+        reward_trajectory = []
+        action_trajectory = []
+        logit_trajectory = []
+
 
         #update network
         if not mqueue.empty():
@@ -59,7 +64,11 @@ def actor(game:str, tqueue: mp.Queue, mqueue: mp.Queue, is_done:mp.Event(), is_p
             for j in range(n_action_repeats - 1):
                 env.step(action)
             next_frame, reward, terminated, truncated, info = env.step(action)
-            trajectory.append((obs, action_probs, clip_reward(reward)))
+
+            obs_trajectory.append(obs)
+            reward_trajectory.append(clip_reward(reward))
+            action_trajectory.append(action)
+            logit_trajectory.append(action_probs)
 
             #ready for next step
             obs = preprocessor.get_obs(next_frame)
@@ -69,7 +78,12 @@ def actor(game:str, tqueue: mp.Queue, mqueue: mp.Queue, is_done:mp.Event(), is_p
                 obs = preprocessor.get_obs(frame)
                 lives = info['lives']
 
-        tqueue.put(trajectory)
+        obs_trajectory = np.stack(obs_trajectory)
+        action_trajectory = np.stack(action_trajectory)
+        reward_trajectory = np.stack(reward_trajectory)
+        logit_trajectory = np.stack(logit_trajectory)
+
+        tqueue.put({"obs":obs_trajectory, "action":action_trajectory, "reward":reward_trajectory, "logits":logit_trajectory})
 
 
         if is_done.is_set():
@@ -85,33 +99,131 @@ def actor(game:str, tqueue: mp.Queue, mqueue: mp.Queue, is_done:mp.Event(), is_p
                 env.close()
                 return
 
-def learner(game:str, tqueue: mp.Queue, mqueues: list[mp.Queue], is_done:mp.Event(), is_paused:mp.Event(), trajectory_length:int, stack_n_frames:int, n_action_repeats:int, episodes:int, print_freq:int):
+def learner(v_network:NeuralNet, p_network:NeuralNet, game:str, tqueue: mp.Queue, mqueues: list[mp.Queue], is_done:mp.Event(), is_paused:mp.Event(), trajectory_length:int, stack_n_frames:int, n_action_repeats:int, episodes:int, print_freq:int, batch_size:int, gamma:float) -> (NeuralNet, NeuralNet):
     process = mp.current_process()
     print("starting learner with pid "+str(process.pid))
     env = EnvUtil.getEnvList([game])[0]
-    network = NeuralNet(stack_n_frames, env.action_space.n)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    v_network.to(device)
+    p_network.to(device)
+    l2_loss = torch.nn.MSELoss()
+
+    #hyperparamters
+    #move these somewhere else
+    rho = 1
+    c = 1
+    value_loss_scaler = 1
+    policy_loss_scaler = 1
+    entropy_loss_scaler = 1
+
+    optimizer = torch.optim.Adam(list(v_network.parameters()) + list(p_network.parameters()), lr=0.0006)
 
     rewards = []
+    losses = []
+    #main training loop
     for i in range(episodes):
 
-        #do useful things that learn
-        print("trajectory queue size {}".format(tqueue.qsize()))
-        time.sleep(1)
+        #create batch
+        batch = []
+        for _ in range(batch_size):
+            #if queue empty, wait
+            while tqueue.empty():
+                time.sleep(1)
+            batch.append(tqueue.get())
+
+        batch_obs = []
+        batch_action = []
+        batch_reward = []
+        batch_logits = []
+
+        for t in batch:
+            batch_obs.append(t["obs"])
+            batch_action.append(t["action"])
+            batch_reward.append(t["reward"])
+            batch_logits.append(t["logits"])
+
+        obs = torch.from_numpy(np.stack(batch_obs)).float().to(device)
+        action = torch.from_numpy(np.stack(batch_action)).long().to(device).squeeze(2)
+        actor_logits = torch.from_numpy(np.stack(batch_logits)).to(device).squeeze(2)
+        reward = torch.from_numpy(np.stack(batch_reward)).to(device).unsqueeze(2)
+
+        rewards.append(torch.mean(reward.float()))
+
+
+
+        #do some learning
+        values = [v_network(obs.select(1, j)) for j in range(trajectory_length)]
+        learner_probs = [p_network(obs.select(1, j)) for j in range(trajectory_length)]
+
+        learner_probs = torch.stack(learner_probs, dim=1).to(device)
+
+        #print("learner: " + str(learner_probs.select(1, 0).shape))
+
+        #used to start recursive v-trace calcs
+        next_v_trace = values[-1]
+        v_traces = []
+        values_plus1 = values.copy()
+        values_plus1.append(values[-1])
+        values_plus1 = torch.stack(values_plus1, dim=1).to(device)
+
+        #calculate V-trace targets and losses
+        for t in reversed(range(trajectory_length)):
+            ratio = learner_probs.select(1, t).gather(1, action.select(1, t)) / actor_logits.select(1, t).gather(1, action.select(1, t))
+            rho_t = torch.clamp(ratio, max=rho)
+            cs = torch.clamp(ratio, max=c)
+            delta = rho_t *(reward.select(1,t) + gamma*values_plus1.select(1, t+1) - values_plus1.select(1, t))
+            v_traces.append(values_plus1.select(1,t) + delta + gamma*cs*(next_v_trace - values_plus1.select(1,t)))
+
+            next_v_trace = v_traces[-1]
+
+        v_traces.reverse()
+
+        #calculate losses (probably redo all of this, it sucks)
+        #value loss
+        value_tensor = torch.stack(values, dim=1).float().to(device)
+        v_trace_tensor = torch.stack(v_traces, dim=1).float().to(device)
+        value_loss = l2_loss(value_tensor, v_trace_tensor)
+
+        #policy loss
+        flat_action = torch.flatten(action, 0, 1)
+        flat_learner_probs = torch.flatten(learner_probs, 0, 1)
+        cross_entropy = F.cross_entropy(flat_learner_probs, flat_action.squeeze(1), reduction="none")
+        cross_entropy = cross_entropy.view_as(v_trace_tensor)
+        policy_loss = torch.mean(cross_entropy * v_trace_tensor.detach())
+
+        #entropy loss
+        entropy_loss = 0.0006 * torch.mean(F.softmax(learner_probs, dim=-1) * F.log_softmax(learner_probs, dim=-1))
+
+
+        total_loss = value_loss * value_loss_scaler + policy_loss * policy_loss_scaler + entropy_loss * entropy_loss_scaler
+
+        losses.append(total_loss.item())
+
+
+
+        #update parameters
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+
+
+
 
         if i % print_freq == 0:
-            print("Trajectory {}| Avg. Reward {:.3f}".format(i, np.mean(rewards[-print_freq:])))
+            print("Trajectory {} | T-queue size {} | Avg. Loss {} | Avg. Reward {:.3f}".format(i, tqueue.qsize(), np.mean(losses[-print_freq:]), torch.mean(torch.stack(rewards[-print_freq:]))))
+            #print("Value Loss {} | Policy Loss {} | Entropy Loss {}".format(value_loss.item(), policy_loss.item(), entropy_loss.item()))
 
         if is_done.is_set():
             env.close()
-            return
+            return p_network, v_network
 
         while is_paused.is_set():
             time.sleep(2)
             if is_done.is_set():
                 env.close()
-                return
+                return p_network, v_network
     env.close()
+    return p_network, v_network
 
 
 class Impala:
@@ -129,13 +241,14 @@ class Impala:
         self.learning_rate = 0.0006
         self.gamma = 0.99
         self.batch_size = 32
-        self.actor_num = 15
+        self.actor_num = 2
         self.learner_num = 1
+        self.max_queue_size = 1000
 
         #global objects
         self.p_network = NeuralNet(self.stack_n_frames, self.env.action_space.n)
-        self.v_network = NeuralNet(self.stack_n_frames, self.env.action_space.n)
-        self.trajectory_queue = mp.Queue()
+        self.v_network = NeuralNet(self.stack_n_frames, 1)
+        self.trajectory_queue = mp.Queue(maxsize=self.max_queue_size)
         self.is_done = mp.Event()
         self.is_done.clear()
         self.is_paused = mp.Event()
@@ -223,7 +336,7 @@ class Impala:
 
         time.sleep(2)
 
-        learner(self.game, self.trajectory_queue, queues, self.is_done, self.is_paused, self.trajectory_length, self.stack_n_frames, self.n_action_repeats, self.episodes, self.print_freq)
+        self.p_network, self.v_network = learner(self.v_network, self.p_network, self.game, self.trajectory_queue, queues, self.is_done, self.is_paused, self.trajectory_length, self.stack_n_frames, self.n_action_repeats, self.episodes, self.print_freq, self.batch_size, self.gamma)
         print("\nTraining finished")
         self.is_done.set()
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import gymnasium
 
 from Algorithms import IMPALA
@@ -8,55 +10,62 @@ from MiscUtil import Preprocessor
 
 if __name__ == '__main__':
     #global hyperparameters
-    episodes = 10
+    episodes = 100
     max_steps = 1000
     n_action_repeats = 4
     stack_n_frames = 4
     obs_size = (106, 140)
-    print_freq = 100
+    print_freq = 10
 
-    algorithm_list = [IMPALA, RandomAction]
+    algorithm_list = ["Impala", "RandomAction"]
+    algorithm_objects = []
 
-    while True:
-        print('1. train all algorithms')
-        print('2. train single algorithm')
-        print('3. run all algorithms')
-        print('4. run single algorithm')
-        print('5. create graph')
-        print('0. exit')
+    #if there are command line arguments, use the arguments instead of interaction with user
+    if len(sys.argv) > 1:
+        #to implement
+        pass
 
-        choice = input('Enter your choice: ')
-        match choice:
-            case '1':
-                gameList = chooseGames()
+    else:
+        while True:
+            print('1. train all algorithms')
+            print('2. train single algorithm')
+            print('3. run all algorithms')
+            print('4. run single algorithm')
+            print('0. exit')
 
-                print(gameList)
-                for game in gameList:
-                    for alg in algorithm_list:
-                        a = alg(game, episodes, max_steps, stack_n_frames, n_action_repeats, print_freq)
-                        print("starting {}".format(a.__str__()))
-                        a.train()
-                        a.save()
-                        print("{} done".format(a.__str__()))
+            choice = input('Enter your choice: ')
+            match choice:
+                case '1':
+                    gameList = chooseGames()
 
-                    impala = Impala(game, episodes, max_steps, stack_n_frames, n_action_repeats, print_freq)
-                    impala.train()
+                    #print(gameList)
+                    for game in gameList:
+                        print("starting game {}".format(game))
+                        for alg in algorithm_list:
+                            a = cls = globals()[alg](game, episodes, max_steps, stack_n_frames, n_action_repeats, print_freq)
+                            print("starting {}".format(a.__str__()))
+                            a.train()
+                            a.save()
+                            algorithm_objects.append(a)
+                            print("{} done".format(a.__str__()))
 
+                        # rand = RandomAction("Asteroids", episodes, max_steps, stack_n_frames, n_action_repeats, print_freq)
+                        # rand.train()
+                        # break
 
-                    #preprocessor = Preprocessor(env.reset()[0], stack_n_frames)
+                        # impala = Impala("Asteroids", episodes, max_steps, stack_n_frames, n_action_repeats, print_freq)
+                        # impala.train()
+                        # algorithm_objects.append(impala)
+                        # break
+
+                case '2':
+                    print('2. train single algorithm')
+                case '3':
+                    print('3. run all algorithms')
+                case '4':
+                    print('4. run single algorithm')
+                case '0':
+                    print('Shutting down')
                     break
-
-
-            case '2':
-                print('2. train single algorithm')
-            case '3':
-                print('3. run all algorithms')
-            case '4':
-                print('4. run single algorithm')
-            case '5':
-                print('5. create graph')
-            case '0':
-                print('Shutting down')
-                break
-            case _:
-                print('Invalid choice, please try again')
+                case _:
+                    print('Invalid choice, please try again')
